411edd1719093a6a330319ca13e0ebadfb1c4ba6
hidarrazka <haidarazka.0807@gmail.com>
2026-02-19 15:44:53 +0700

    19 Febuari 2026

---
+++ id/ac/polban/service/Main.java
@@ -0,0 +1,33 @@
+package id.ac.polban.service;
+
+import id.ac.polban.employee.model.*;
+
+
+
+public class Main {
+    public static void main(String[] args){
+        Department itDept = new Department("IT");
+        Department hrDept = new Department("HR");
+        Department cnsltnDept = new Department("CS");
+        EmploymentType fulltime = new EmploymentType("FUll TIME");
+        EmploymentType partime = new EmploymentType("PART TIME");
+        EmploymentType contract = new EmploymentType("CONTRACT");
+
+        
+        Employee emp1 = new Employee(1,"Haidar", itDept,fulltime ,45000000 );
+        // Membuat Service
+        EmployeeService service = new EmployeeService();
+
+        // Menambahkan Employee
+        service.addEmployee(emp1);
+
+        System.out.println("=== Data Awal Employee ===");
+        service.displayAllEmployees();
+
+        // Naikkan gaji
+        service.raiseSalary(1, 10);
+
+        System.out.println("=== Setelah Kenaikan Gaji 10% untuk ID 1 ===");
+        service.displayAllEmployees();
+    }
+}
